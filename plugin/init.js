3edda9cd18d32c94289702de1faa573361d43cac
peaceshi <peaceshi@users.noreply.github.com>
2020-02-17 09:44:21 +0000

    deploy: f13ec684e05676c60acafb0eaea718ca4533ba99

--- plugin/init.js
+++ plugin/init.js
@@ -23,7 +23,7 @@ function registServiceWorker() {
 				}
 				// 注册 Service Worker
 				navigator.serviceWorker
-					.register("/GameProgrammingPatterns/service-worker.js", { scope: "/GameProgrammingPatterns" })
+					.register("/GameProgrammingPatterns/service-worker.js")
 					.then(
 						registration => {
 							// 注册成功
