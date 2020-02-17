05a123c2e9a14ca04defb29a6d8fb6ca7cbe4876
peaceshi <peaceshi@users.noreply.github.com>
2020-02-17 09:04:25 +0000

    deploy: 04f9756257cc7657780ccb6a2b723a81b1f7d1ac

--- plugin/init.js
+++ plugin/init.js
@@ -1,23 +1,47 @@
-
 function patchImgElementTag() {
-    var elements = document.getElementsByTagName("img");
-    for(var i=0; i < elements.length; i+=1) {
-        var element = elements.item(i);
-        if (element.tagName.toLowerCase() == "img") {
+	var elements = document.getElementsByTagName("img");
+	for (var i = 0; i < elements.length; i += 1) {
+		var element = elements.item(i);
+		if (element.tagName.toLowerCase() == "img") {
 			element.className = "lazyload";
 			element.getAttribute("src");
-			element.setAttribute("data-src",element.getAttribute("src"));
+			element.setAttribute("crossorigin","");
+			element.setAttribute("data-src", element.getAttribute("src"));
 			element.removeAttribute("src");
-        }
-    }
+		}
+	}
+}
+//https://lavas-project.github.io/pwa-book
+function registServiceWorker() {
+	// Check that service workers are supported
+	if ("serviceWorker" in navigator) {
+		// 在 load 事件触发后注册 Service Worker，确保 Service Worker 的注册不会影响首屏速度
+		window.addEventListener("load", () => {
+			navigator.serviceWorker.getRegistrations().then(regs => {
+				for (let reg of regs) {
+					reg.unregister();
+				}
+				// 注册 Service Worker
+				navigator.serviceWorker
+					.register("/service-worker.js", { scope: "/" })
+					.then(
+						registration => {
+							// 注册成功
+							console.log(
+								"ServiceWorker registration successful with scope: ",
+								registration.scope
+							);
+						},
+						err => {
+							// 注册失败:(
+							console.warn("ServiceWorker registration failed: ", err);
+						}
+					);
+			});
+		});
+	}
 }
 
 patchImgElementTag();
+registServiceWorker();
 
-// Check that service workers are supported
-if ('serviceWorker' in navigator) {
-	// Use the window load event to keep the page load performant
-	window.addEventListener('load', () => {
-	  navigator.serviceWorker.register('/service-worker.js');
-	});
-  }
--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,19 @@
 console.log("Hello from service-worker.js");
-
 importScripts(
 	"https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
 );
-
 if (workbox) {
 	console.log(`Yay! Workbox is loaded 🎉`);
 } else {
 	console.log(`Boo! Workbox didn't load 😬`);
 }
+const { strategies } = workbox;
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
+const {ExpirationPlugin} = workbox.expiration;
 
 // This will trigger the importScripts() for workbox.strategies and its dependencies:
-const { strategies } = workbox;
-
 self.addEventListener("fetch", event => {
 	if (event.request.url.endsWith(".png")) {
 		// Using the previously-initialized strategies will work as expected.
@@ -31,15 +32,57 @@ self.addEventListener("fetch", event => {
 	}
 });
 
-const { registerRoute } = workbox.routing;
-const { CacheFirst } = workbox.strategies;
-const { CacheableResponse } = workbox.cacheableResponse;
-
 registerRoute(
 	/\.(?:png|jpg|jpeg|svg|gif)$/,
 	new CacheFirst({
 		cacheName: "image-cache",
-		plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })]
+		plugins: [
+			new CacheableResponse({ statuses: [0, 200] }),
+			new ExpirationPlugin({
+				// 对图片资源缓存 1 星期
+				maxAgeSeconds: 7 * 24 * 60 * 60,
+				// 匹配该策略的图片最多缓存 10 张
+				maxEntries: 100
+			})
+		]
 	})
 );
 
+registerRoute(
+	/\.css$/,
+	new CacheFirst({
+		cacheName: "image-cache",
+		plugins: [
+			new CacheableResponse({ statuses: [0, 200] }),
+			new ExpirationPlugin({
+				// 对资源缓存 1 天
+				maxAgeSeconds: 1 * 24 * 60 * 60,
+				// 匹配该策略最多缓存 10 条
+				maxEntries: 100
+			})
+		]
+	})
+);
+registerRoute(
+	/\.html$/,
+	new CacheFirst({
+		cacheName: "html-cache",
+		plugins: [
+			new CacheableResponse({ statuses: [0, 200] }),
+			new ExpirationPlugin({
+				// 对资源缓存 1 天
+				maxAgeSeconds: 1 * 24 * 60 * 60,
+				// 匹配该策略最多缓存 10 条
+				maxEntries: 100
+			})
+		]
+	})
+);
+
+registerRoute(
+	/^https:\/\/cdn.staticfile.org\*.min.*$/,
+	new CacheFirst({
+		cacheName: "CDN-cache",
+		plugins: [new CacheableResponse({ statuses: [0, 200] })]
+	})
+);
